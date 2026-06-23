6038232e2fc6d7e484afacb008dae0ceee983732
Dennco Holding Company <96162801+DenncoLABS@users.noreply.github.com>
2026-06-23 19:50:56 -0400

    Add maritime node activation state

--- client/src/modules/maritime/state/maritime.store.ts
+++ client/src/modules/maritime/state/maritime.store.ts
@@ -1,4 +1,7 @@
 import { create } from 'zustand';
+import { MARITIME_NODES } from '../data/maritimeInfrastructure';
+
+const ALL_MARITIME_NODE_IDS = MARITIME_NODES.map((node) => node.id);
 
 export interface MaritimeFilters {
   speedMin: number;
@@ -13,6 +16,16 @@ interface MaritimeState {
   setFilter: <K extends keyof MaritimeFilters>(key: K, value: MaritimeFilters[K]) => void;
   selectedMmsi: number | null;
   setSelectedMmsi: (mmsi: number | null) => void;
+  showPorts: boolean;
+  setShowPorts: (show: boolean) => void;
+  showInstallations: boolean;
+  setShowInstallations: (show: boolean) => void;
+  showMaritimeNodes: boolean;
+  setShowMaritimeNodes: (show: boolean) => void;
+  activeMaritimeNodeIds: string[];
+  toggleMaritimeNode: (nodeId: string) => void;
+  activateAllMaritimeNodes: () => void;
+  clearMaritimeNodes: () => void;
 }
 
 export const useMaritimeStore = create<MaritimeState>((set) => ({
@@ -26,4 +39,19 @@ export const useMaritimeStore = create<MaritimeState>((set) => ({
   setFilter: (key, value) => set((state) => ({ filters: { ...state.filters, [key]: value } })),
   selectedMmsi: null,
   setSelectedMmsi: (mmsi) => set({ selectedMmsi: mmsi }),
+  showPorts: true,
+  setShowPorts: (showPorts) => set({ showPorts }),
+  showInstallations: true,
+  setShowInstallations: (showInstallations) => set({ showInstallations }),
+  showMaritimeNodes: true,
+  setShowMaritimeNodes: (showMaritimeNodes) => set({ showMaritimeNodes }),
+  activeMaritimeNodeIds: [],
+  toggleMaritimeNode: (nodeId) =>
+    set((state) => ({
+      activeMaritimeNodeIds: state.activeMaritimeNodeIds.includes(nodeId)
+        ? state.activeMaritimeNodeIds.filter((id) => id !== nodeId)
+        : [...state.activeMaritimeNodeIds, nodeId],
+    })),
+  activateAllMaritimeNodes: () => set({ activeMaritimeNodeIds: ALL_MARITIME_NODE_IDS }),
+  clearMaritimeNodes: () => set({ activeMaritimeNodeIds: [] }),
 }));
