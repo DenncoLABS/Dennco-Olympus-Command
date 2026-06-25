c2c47316937ac2c75a106c29c72d25231792961a
Dennco Holding Company <96162801+DenncoLABS@users.noreply.github.com>
2026-06-24 21:58:46 -0400

    Add map widget domain exports

---
+++ client/src/widgets/map-widgets/index.ts
@@ -0,0 +1,11 @@
+export { flightMapWidgetManifest } from './flight-widgets';
+export type { FlightMapWidgetDefinition, FlightMapWidgetId } from './flight-widgets';
+
+export { maritimeMapWidgetManifest } from './maritime-widgets';
+export type { MaritimeMapWidgetDefinition, MaritimeMapWidgetId } from './maritime-widgets';
+
+export { dotMapWidgetManifest } from './dot-widgets';
+export type { DotMapWidgetDefinition, DotMapWidgetId } from './dot-widgets';
+
+export { cyberMapWidgetManifest } from './cyber-widgets';
+export type { CyberMapWidgetDefinition, CyberMapWidgetId } from './cyber-widgets';
