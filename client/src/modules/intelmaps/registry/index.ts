b0edcb4f20ad55eb56031df54a29d035753fedcf
Dennco Holding Company <96162801+DenncoLABS@users.noreply.github.com>
2026-06-25 22:13:33 -0400

    Add Intel Maps registry barrel

---
+++ client/src/modules/intelmaps/registry/index.ts
@@ -0,0 +1,5 @@
+export * from './types';
+export * from '../layers/layerCatalog';
+export * from '../feeds/feedCatalog';
+export * from '../widgets/widgetCatalog';
+export * from '../workflows/workflowCatalog';
