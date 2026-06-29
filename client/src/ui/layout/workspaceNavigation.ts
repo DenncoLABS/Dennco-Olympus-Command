a305b7785cdb4a41c005305a6af954cadb4e44b1
Dennco Holding Company <96162801+DenncoLABS@users.noreply.github.com>
2026-06-29 07:06:44 -0400

    Export workspace event publisher

--- client/src/ui/layout/workspaceNavigation.ts
+++ client/src/ui/layout/workspaceNavigation.ts
@@ -3,4 +3,5 @@ export * from './openOlympusWorkspace';
 export * from './openDeskOrWorkspace';
 export * from './workspaceEvents';
 export * from './publishDeskViewSync';
+export * from './publishWorkspaceEvents';
 export * from './coreDeskViews';
