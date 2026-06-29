098587de73dc90aa7a86c1f1f587d8265aa53a2b
Dennco Holding Company <96162801+DenncoLABS@users.noreply.github.com>
2026-06-29 07:05:28 -0400

    Add shared workspace event publisher

---
+++ client/src/ui/layout/publishWorkspaceEvents.ts
@@ -0,0 +1,10 @@
+import {
+  OLYMPUS_WORKSPACE_LAUNCH_EVENT,
+  OLYMPUS_WORKSPACE_OPENED_EVENT,
+  type OlympusWorkspaceEventDetail,
+} from './workspaceEvents';
+
+export function publishWorkspaceEvents(detail: OlympusWorkspaceEventDetail) {
+  window.dispatchEvent(new CustomEvent(OLYMPUS_WORKSPACE_OPENED_EVENT, { detail }));
+  window.dispatchEvent(new CustomEvent(OLYMPUS_WORKSPACE_LAUNCH_EVENT, { detail }));
+}
