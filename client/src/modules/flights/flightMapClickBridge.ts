9cf20896a0b4c65009941e5c1db73202b95880f8
Dennco Holding Company <96162801+DenncoLABS@users.noreply.github.com>
2026-06-30 06:37:13 -0400

    Loosen flight map click bridge types

--- client/src/modules/flights/flightMapClickBridge.ts
+++ client/src/modules/flights/flightMapClickBridge.ts
@@ -1,4 +1,4 @@
-import maplibregl from 'maplibre-gl';
+import * as maplibregl from 'maplibre-gl';
 import { useFlightsStore } from './state/flights.store';
 
 const BOOT_KEY = '__olympusFlightMapClickBridgeReady';
@@ -6,8 +6,8 @@ const PATCH_KEY = '__olympusFlightMapClickFirePatched';
 const FEATURE_LAYERS = ['aircraft-click-target', 'aircraft-points'];
 
 type ScopedWindow = Window & { [BOOT_KEY]?: boolean };
-type PatchedMapPrototype = typeof maplibregl.Map.prototype & { [PATCH_KEY]?: boolean };
-type MapClickPayload = { point?: { x: number; y: number }; target?: maplibregl.Map };
+type LooseMap = maplibregl.Map & { fire: (...args: unknown[]) => unknown; [PATCH_KEY]?: boolean };
+type MapClickPayload = { point?: { x: number; y: number } };
 
 function selectRenderedAircraft(map: maplibregl.Map, point: { x: number; y: number }): void {
   const box: [[number, number], [number, number]] = [[point.x - 28, point.y - 28], [point.x + 28, point.y + 28]];
@@ -17,17 +17,18 @@ function selectRenderedAircraft(map: maplibregl.Map, point: { x: number; y: numb
 }
 
 function patchMapClicks(): void {
-  const prototype = maplibregl.Map.prototype as PatchedMapPrototype;
+  const prototype = maplibregl.Map.prototype as unknown as LooseMap;
   if (prototype[PATCH_KEY]) return;
 
   const originalFire = prototype.fire;
-  prototype.fire = function patchedFire(type: string, data?: unknown) {
+  prototype.fire = function patchedFire(this: maplibregl.Map, ...args: unknown[]) {
+    const [type, data] = args;
     if (type === 'click') {
       const payload = data as MapClickPayload | undefined;
-      if (payload?.point) selectRenderedAircraft(this as maplibregl.Map, payload.point);
+      if (payload?.point) selectRenderedAircraft(this, payload.point);
     }
-    return originalFire.call(this, type, data);
-  } as typeof prototype.fire;
+    return originalFire.apply(this, args);
+  };
 
   prototype[PATCH_KEY] = true;
 }
