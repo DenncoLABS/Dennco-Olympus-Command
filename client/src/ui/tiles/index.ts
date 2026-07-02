922ee199b39cdd2d0c5607e0f745a07a538615f7
Dennco Holding Company <96162801+DenncoLABS@users.noreply.github.com>
2026-07-02 10:23:35 -0400

    Export TileSpace MVP building blocks

---
+++ client/src/ui/tiles/index.ts
@@ -0,0 +1,4 @@
+export { tileRegistry, getTileRegistryItem, getTilesForApp } from './tileRegistry';
+export type { TileKind, TileLayout, TileRegistryItem } from './tileRegistry';
+export { TileRuntimeCard } from './TileRuntimeCard';
+export { TileQuadPreview } from './TileQuadPreview';
