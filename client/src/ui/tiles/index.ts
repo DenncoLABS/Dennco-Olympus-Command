a845766e366dedc595662ba1ccbd832d00fb2a4c
Dennco Holding Company <96162801+DenncoLABS@users.noreply.github.com>
2026-07-02 11:16:15 -0400

    Export TileSpace preview focus key

--- client/src/ui/tiles/index.ts
+++ client/src/ui/tiles/index.ts
@@ -4,6 +4,7 @@ export { TileRuntimeCard } from './TileRuntimeCard';
 export {
   TILESPACE_PREVIEW_SELECTED_TILE_KEY,
   TILESPACE_PREVIEW_QUAD_KEY,
+  TILESPACE_PREVIEW_FOCUS_KEY,
   readTileSpacePreviewString,
   readTileSpacePreviewBoolean,
   writeTileSpacePreviewString,
